60248fef741090c656734088d27f9bf049b2c2d8
ivan_markov <zumkeee@gmail.com>
2021-07-13 18:51:18 +0400

    Hexlet

--- src/main/java/hexlet/code/game/Progression.java
+++ src/main/java/hexlet/code/game/Progression.java
@@ -6,7 +6,7 @@ public class Progression {
 
     public static void progressionResult() {
         final int firstNum = 10;
-        final int secondNum = 5;
+        final int secondNum = 6;
         int lengthProgression = firstNum + (int) (Math.random() * ((secondNum - firstNum) + 1));
         int point = (int) (Math.random() * lengthProgression);
         final int maxNum = 50;
